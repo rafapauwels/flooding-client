ed660156b3f4b82cb77b4205be8af6f121297f4b
Rafael Pauwels <rafapauwels@gmail.com>
2019-08-05 01:28:11 -0300

    Adicionando TCP

--- src/cliente/core.clj
+++ src/cliente/core.clj
@@ -1,8 +1,11 @@
 (ns cliente.core)
+(require '[clojure.java.io :as io])
 (import '[java.net DatagramSocket
           DatagramPacket
           InetSocketAddress
-          InetAddress])
+          InetAddress
+          Socket]
+        '[java.io StringWriter])
 
 (comment  (def socket (DatagramSocket. 9442)))
 (def alvos [{:endereco-ip "localhost" :porta 9443}]) ; Usar peers da cloud
@@ -30,12 +33,44 @@
         pacote (DatagramPacket. payload tamanho-requisicao endereco)]
     (.send socket pacote)))
 
-(defn solicitar-arquivo
+(defn solicita-arquivo
   "TTL default 3, monta e envia requisição usando funções auxiliares"
   ([nome-arquivo]
-   (solicitar-arquivo nome-arquivo 3))
+   (solicita-arquivo nome-arquivo 3))
   ([nome-arquivo ttl]
    (let [servidor-alvo (escolhe-servidor-alvo alvos)
          query-pesquisa (constroi-query nome-arquivo ttl)]
      (println (str "Enviando pesquisa por " nome-arquivo " para " servidor-alvo))
      (enviar-query socket query-pesquisa servidor-alvo))))
+
+(defn recebe-requisicao-ret
+  [^DatagramSocket socket]
+  (let [buffer (byte-array 512)
+        pacote (DatagramPacket. buffer 512)]
+    (println "Recebido retorno da solicitação")
+    (.receive socket pacote)
+    (String. (.getData pacote)
+             0 (.getLength pacote))))
+
+(defn loop-recebimento-requisicoes-ret 
+  [socket f]
+  (future (while true (f (recebe-requisicao-ret socket)))))
+
+(defn solicita-transferencia
+  [endereco-tcp porta caminho-arquivo]
+  (with-open [sock (Socket. endereco-tcp porta)
+              writer (io/writer sock)
+              reader (io/reader sock)
+              resposta (StringWriter.)]
+    (.append writer (str "GET " caminho-arquivo "\n"))
+    (.flush writer)
+    (io/copy reader resposta)
+    (str resposta)))
+
+(defn trata-requisicao-ret
+  [requisicao-ret]
+  (println (str requisicao-ret))
+  (let [ret-mapeado (clojure.edn/read-string requisicao-ret)
+        endereco-tcp (:endereco-tcp ret-mapeado)
+        caminho-arquivo (:caminho-arquivo ret-mapeado)]
+    (solicita-transferencia endereco-tcp 9443 caminho-arquivo)))
